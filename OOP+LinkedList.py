71bf0192f3efe429389b9fcd1a6bd203b9d59413
MrLeeYH <103474303+MrLeeYH@users.noreply.github.com>
2022-08-24 10:03:30 +0800

    Create OOP+LinkedList.py

---
+++ OOP+LinkedList.py
@@ -0,0 +1,6 @@
+# using OOP + linkedlist, create a data structure that can show the list of pull request and help Boss Gi decide which pull request to process next
+# add_request(name) method, which adds new request by name
+# display_all() method, which shows all pull requests
+# resolve() method, which shows who is next to be resolved
+# any other methods you need
+
